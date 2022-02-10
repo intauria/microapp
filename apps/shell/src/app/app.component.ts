f403d9b4e39b2bb2c10c91665f1bdcd445335886
michael.zikes <michael.zikes@intauria.com>
2022-02-10 09:28:18 +0100

    init: microapp-platform workspace

---
+++ apps/shell/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'microapp-platform-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shell';
+}
