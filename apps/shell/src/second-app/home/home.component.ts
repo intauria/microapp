e41873778c4d9efc1cd5253fbff291af815ad91d
michael.zikes <michael.zikes@intauria.com>
2022-02-11 19:35:11 +0100

    docs: update demo app

--- apps/shell/src/second-app/home/home.component.ts
+++ apps/shell/src/second-app/home/home.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'microapp-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HomeComponent {
 }
--- apps/shell/src/second-app/use-case/use-case.component.ts
+++ apps/shell/src/second-app/use-case/use-case.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'microapp-use-case',
   templateUrl: './use-case.component.html',
   styleUrls: ['./use-case.component.scss']
 })
-export class UseCaseComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class UseCaseComponent {
 }
