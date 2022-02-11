db928a861aa1f104d903a45aea40dfc613f0819b
michael.zikes <michael.zikes@intauria.com>
2022-02-11 16:52:03 +0100

    feat: messaging api

---
+++ libs/microapp/src/lib/messaging/event-messaging.ts
@@ -0,0 +1,47 @@
+export type MessageFn<T> = (payload?: T, config?: { app?: string, type?: string }) => boolean;
+
+export function payload<T>() {
+  return {} as T;
+}
+
+export let MESSAGE_PREFIX = '@angular-architects/microapp@event.type:'
+
+export function overwriteMessagePrefix(prefix: string) {
+  MESSAGE_PREFIX = prefix;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function createMessage<T>(type: string, app?: string, payload?: T): MessageFn<T> {
+  return (payload?: T, config?: { app?: string, type?: string }) => document.dispatchEvent(new CustomEvent(
+    `${ MESSAGE_PREFIX }${ config?.type || type }`,
+    { detail: {
+      app: config?.app || app,
+      payload
+    }}
+  ));
+}
+
+export type ObserverFn<T> = (payload: T, app?: string, ev?: CustomEvent) => void;
+
+export interface MessageListener<T> {
+  subscribe: (callback: ObserverFn<T>) => void;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function messageListener<T>(type: string, app?: string, payload?: T): MessageListener<T> {
+  return {
+    subscribe: (callback: ObserverFn<T>) => subscribeMessage<T>(type, callback, { app })
+  };
+}
+
+export function subscribeMessage<T>(type: string, callback: ObserverFn<T>, config?: { app?: string }): void {
+  document.addEventListener(
+    `${ MESSAGE_PREFIX }${ type }`,
+    ev => ((config?.app && (ev as CustomEvent)?.detail?.app === config.app) || ! config?.app) &&
+      callback(
+        (ev as CustomEvent)?.detail?.payload,
+        (ev as CustomEvent)?.detail?.app,
+        (ev as CustomEvent)
+      )
+  );
+}
