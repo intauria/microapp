44e6ec63c6c70594ca4978e901dea735c8d6a6b9
mikezks <23002661+mikezks@users.noreply.github.com>
2022-10-25 16:53:15 +0200

    fix: react poc

--- apps/react-mfe/src/main.tsx
+++ apps/react-mfe/src/main.tsx
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('mfe-react') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <StrictMode>
     <CustomRouter history={customHistory} microApp={'mfe-react'}>
       <App />
     </CustomRouter>
